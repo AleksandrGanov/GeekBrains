301f995d3a7eb0de58230569872821cb93b3ed02
Ganov Aleksandr <gaa8008@gmail.com>
2019-10-27 16:07:47 +1200

    BaseCommit

---
+++ 04. C# Уровень 1/C# Solutions/CSharp_Level01/Lesson_01. HW_Task_01/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+/* Описание задания:
+Ганов Александр Анатольевич
+====================
+Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). 
+В результате вся информация выводится в одну строчку:
+а) используя  склеивание;
+б) используя форматированный вывод;
+в) используя вывод со знаком $.
+ */
+
+namespace Lesson_01.HW_Task_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = AskUser("Введите Ваше имя");
+            string surname = AskUser("Введите Вашу фамилию");
+            int age = Convert.ToInt32(AskUser("Введите Ваш возраст"));
+            int height = Convert.ToInt32(AskUser("Введите Ваш рост"));
+            int weight = Convert.ToInt32(AskUser("Введите Ваш вес"));
+
+            // вариант "а" - склеивание
+            Console.WriteLine("\nАнкета, вар \"а\":" +
+                "\n================" +
+                "\nИмя: " + name +
+                "\nФамилия: " + surname +
+                "\nВозраст: " + age +
+                "\nРост: " + height +
+                "\nВес: " + weight +
+                "\n================");
+
+            // вариант "б" - форматированный вывод
+            Console.WriteLine("\nАнкета, вар \"б\":\n================\nИмя: {0}\nФамилия: {1}" +
+                "\nВозраст: {2}\nРост: {3}\nВес: {4}\n================",
+                name, surname, age, height, weight);
+
+            // вариант "в" - вывод со знаком $ (интерполяция строк)
+            Console.WriteLine($"\nАнкета, вар \"в\":\n================\nИмя: {name}\nФамилия: {surname}" +
+                $"\nВозраст: {age}\nРост: {height}\nВес: {weight}\n================");
+
+            Console.WriteLine("\nокончание вывода");
+            Console.ReadLine();
+        }
+
+        static string AskUser(string str)
+        {
+            Console.Write(str + ": ");
+            return Console.ReadLine();
+        }
+    }
+}
