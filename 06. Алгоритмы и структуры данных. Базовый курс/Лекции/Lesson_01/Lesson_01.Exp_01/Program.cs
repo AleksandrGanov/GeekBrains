f0ddd917f39e88a87a185181a2dbcb6694b3e953
Ganov Aleksandr <gaa8008@gmail.com>
2020-02-24 01:00:09 +1200

    BaseCommit

--- 06. Алгоритмы и структуры данных. Базовый курс/Лекции/Lesson_01/Lesson_01.Exp_01/Program.cs
+++ 06. Алгоритмы и структуры данных. Базовый курс/Лекции/Lesson_01/Lesson_01.Exp_01/Program.cs
@@ -1,11 +1,9 @@
-﻿// Создание класса
-
-using System;
+﻿using System;
 
 using static mlConsole.GetData;
 using static System.Math;
 
-namespace Lesson01_Exp01
+namespace Lesson01.Exp01
 {
     class Program
     {
@@ -27,21 +25,21 @@ namespace Lesson01_Exp01
                 y = GetValueDouble("Введите координату y3: ")
             };
 
-            Console.WriteLine($"Дистанция: {Distance(p1, p2)}");
-            Console.WriteLine($"Периметр треугольника: {TrianglePerimetr(p1, p2, p3)}");
+            Console.WriteLine($"Дистанция: {Distance(p1, p2):F2}");
+            Console.WriteLine($"Периметр треугольника: {TrianglePerimetr(p1, p2, p3):F2}");
             Console.ReadLine();
             return 0;
         }
 
         static double Distance(Point p1, Point p2)
         {
-            return Round(Sqrt(Pow(p1.x - p2.x, 2) + Pow(p1.y - p2.y, 2)), 2);
+            return Sqrt(Pow(p1.x - p2.x, 2) + Pow(p1.y - p2.y, 2));
         }
         static double TrianglePerimetr(Point p1, Point p2, Point p3)
         {
-            return Round(Sqrt(Pow(p1.x - p2.x, 2) + Pow(p1.y - p2.y, 2)), 2)
-                  + Round(Sqrt(Pow(p1.x - p3.x, 2) + Pow(p1.y - p3.y, 2)), 2)
-                  + Round(Sqrt(Pow(p2.x - p3.x, 2) + Pow(p2.y - p3.y, 2)), 2);
+            return Sqrt(Pow(p1.x - p2.x, 2) + Pow(p1.y - p2.y, 2))
+                  + Sqrt(Pow(p1.x - p3.x, 2) + Pow(p1.y - p3.y, 2))
+                  + Sqrt(Pow(p2.x - p3.x, 2) + Pow(p2.y - p3.y, 2));
         }
     }
     struct Point
