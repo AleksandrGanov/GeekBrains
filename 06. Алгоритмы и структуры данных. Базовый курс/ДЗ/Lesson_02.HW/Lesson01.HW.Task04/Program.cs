4250703be15818bf45ac218df29819ec7535581d
Ganov Aleksandr <gaa8008@gmail.com>
2020-02-26 01:00:17 +1200

    BaseCommit

---
+++ 06. Алгоритмы и структуры данных. Базовый курс/ДЗ/Lesson_02.HW/Lesson01.HW.Task04/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Описание задания:
+Ганов Александр Анатольевич
+====================
+Написать программу нахождения корней заданного квадратного уравнения
+*/
+
+using System;
+
+using static mlConsole.GetData;
+
+namespace Lesson01.HW.Task04
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a = GetValueDouble("Введите коэффициент #1: ");
+            double b = GetValueDouble("Введите коэффициент #2: ");
+            double c = GetValueDouble("Введите коэффициент #3: ");
+            double x1, x2;
+
+            Console.WriteLine($"Решаем уравнение типа: {a}x^2+{b}x+{c}=0");
+            
+            double disk = Math.Pow(b, 2) - 4 * a * c;
+            if (disk > 0)
+            {
+                double d = Math.Sqrt(disk);
+                x1 = (d - b) / (2 * a);
+                x2 = (-d - b) / (2 * a);
+                Console.WriteLine($"Корни уравнения: x1={x1}, x2={x2}");
+            }
+            else Console.WriteLine("Уравнение не имеет корней");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 06. Алгоритмы и структуры данных. Базовый курс/ДЗ/Lesson_02.HW/Lesson01.HW.Task_01/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Описание задания:
+Ганов Александр Анатольевич
+====================
+Ввести вес и рост человека. Рассчитать и вывести индекс массы тела по формуле I=m/(h*h); где m-масса тела в килограммах, h - рост в метрах
+ */
+
+using System;
+
+using static mlConsole.GetData;
+
+namespace Lesson01.HW.Task01
+{
+    class Program
+    {
+        static void Main()
+        {
+            uint rost = GetValueUInt("Введите рост, см: ");
+            uint massa = GetValueUInt("Введите массу тела, кг: ");
+            Console.WriteLine($"Ваш ИМТ: {massa / Math.Pow(rost / 100d, 2):F0}");
+            Console.ReadLine();
+        }
+    }
+}
