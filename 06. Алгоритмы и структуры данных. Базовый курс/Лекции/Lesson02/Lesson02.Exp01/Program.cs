50fd4cda19e84e8dd0da3e38d49bf7b5cc58e27b
Ganov Aleksandr <gaa8008@gmail.com>
2020-02-28 01:00:16 +1200

    BaseCommit

---
+++ 06. Алгоритмы и структуры данных. Базовый курс/Лекции/Lesson02/Lesson02.Exp01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Генератор псеводослучайных чисел
+
+using System;
+
+using static mlConsole.GetData;
+
+namespace Lesson01.Exp10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 1, a = 2, b = 3, m = 100;
+            int modulus = GetValueInt("Введите кол-во чисел для генерации: ");
+
+            for (int i = 0; i < modulus; i++)
+            {
+                x = (a * x + b) % m;
+                Console.Write($"{x:F0} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
